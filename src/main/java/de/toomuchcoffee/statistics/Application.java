94dc9f15893892f6c6729604b30d1511e4bb0b9f
toomuchcoffee <geraldsander@gmx.de>
2018-06-09 17:52:27 +0200

    moves Application after reorganizing packages

--- src/main/java/de/toomuchcoffee/statistics/Application.java
+++ src/main/java/de/toomuchcoffee/statistics/Application.java
@@ -1,4 +1,4 @@
-package de.toomuchcoffee;
+package de.toomuchcoffee.statistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
